8f97fa23352e1b635269b36b5e7c5476746d0af7
TseTTC <tsettcv@gmail.com>
2021-04-12 00:55:03 +0800

    12 Apr update

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Completed  
  -9 squares
  -click function and adding of values to array
+
+ 4/12/2021
+ Completed:
+ -Display of X and O's when player clicks
+
+ To Do:
+ -adding functional win lose algorithm
+ -CSS update to look more fresh and new
+ -AI bot (Optional)
--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 /* DOM */
-
+let player = "X"
 CELL = document.getElementsByClassName('cell');
 
+/* Creating Array to hold current state of game*/
 var board = Array(9).fill(" ")
 console.log(board)
 
+/* Factory to add user input */
 const gameBoard = (()=> { 
     const playerMove = (squareNum,marker) => board.splice(squareNum, 1, marker)
     return {
         playerMove
     };
+ 
+})();
+/*Factory to push user input into HTML */
+const playerCurrentX = (()=> {
+    const displayChange = (squareNum, marker) => document.getElementById("cell-" + squareNum).innerHTML = marker
+    return {
+        displayChange
+    }
+})();
+const playerCurrentO = (()=> {
+    const displayChange = (squareNum, marker) => document.getElementById("cell-" + squareNum).innerHTML = marker
+    return {
+        displayChange
+    }
 })();
 
-
+/* Changing player after every click and linking with other functions */
 var test = function(){
     var value = this.getAttribute("value");
-    gameBoard.playerMove(value,"O")
+    if (player === "X"){
+        gameBoard.playerMove(value,"X")
+        playerCurrentX.displayChange(value, "X");
+        player = "O"
+        console.log()   
+    } else {
+        gameBoard.playerMove(value,"O")
+        playerCurrentO.displayChange(value, "O");
+        player = "X"
+        console.log()
+    }
+    
     console.log(board)
     return board
 }
+/* addEvenListener to get clicks */
 for (var i = 0; i < CELL.length; i++) {
     CELL[i].addEventListener('click', test, false);
 }
