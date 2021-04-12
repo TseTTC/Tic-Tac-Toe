d3831a9ff5bb0271be064d0503a8694f177bf0ef
TseTTC <tsettcv@gmail.com>
2021-04-12 17:07:49 +0800

    12 Apr 2021

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-4/5/2021:
-Completed  
- -9 squares
- -click function and adding of values to array
+4/5/2021: <br />
+Completed  <br />
+ -9 squares <br />
+ -click function and adding of values to array <br />
 
- 4/12/2021
+ 4/12/2021 <br />
+ Completed: <br />
+ -Display of X and O's when player clicks <br />
+
+ To Do: <br />
+ -adding functional win lose algorithm <br />
+ -CSS update to look more fresh and new <br />
+ -AI bot (Optional) <br />
+
+ 4/13/2021 <br />
  Completed:
- -Display of X and O's when player clicks
-
- To Do:
- -adding functional win lose algorithm
- -CSS update to look more fresh and new
- -AI bot (Optional)
+ -Functional win lose check <br />
+ -Functional end of game when 3 X's or O's are in place <br />
--- script.js
+++ script.js
@@ -2,53 +2,200 @@
 let player = "X"
 CELL = document.getElementsByClassName('cell');
 
+
 /* Creating Array to hold current state of game*/
 var board = Array(9).fill(" ")
 console.log(board)
-
+var gameState = "playing"
 /* Factory to add user input */
 const gameBoard = (()=> { 
     const playerMove = (squareNum,marker) => board.splice(squareNum, 1, marker)
+    const reset = () => board = Array(9).fill(" ")
     return {
-        playerMove
+        playerMove,
+        reset
     };
- 
+    
 })();
+function gameStatus() {
+    if(gameState === "playing"){
+        gameState = "end"
+        for (var i = 0; i < CELL.length; i++) {
+            CELL[i].removeEventListener('click', test, false);
+        }
+    }
+    else if (gameState === "end"){
+        gameState = "playing"
+        for (var i = 0; i < CELL.length; i++) {
+            CELL[i].addEventListener('click', test, false);
+        }
+    }
+}
 /*Factory to push user input into HTML */
-const playerCurrentX = (()=> {
-    const displayChange = (squareNum, marker) => document.getElementById("cell-" + squareNum).innerHTML = marker
+const playerCurrent = (()=> {
+    const displayChange = (squareNum, marker) => document.getElementById("cell-" + squareNum).innerHTML = marker;
+    ;
     return {
-        displayChange
+        displayChange,
     }
 })();
-const playerCurrentO = (()=> {
-    const displayChange = (squareNum, marker) => document.getElementById("cell-" + squareNum).innerHTML = marker
-    return {
-        displayChange
+var resetBoard = function resetBoard() {
+    for(i = 0; i <9; i++){
+        document.getElementById("cell-" + i).innerHTML = " ";
     }
-})();
+    gameBoard.reset()
+    gameStatus()
+}
+function check(squareNum, marker) {
+    console.log(squareNum)
+    if (squareNum == 0){
+        if (board[1] == marker && board[2] == marker){
+            console.log("You Win!")
+            gameStatus()
+
+        } else if (board[3] == marker && board[6] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[4] == marker && board[8] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+    else if (squareNum == 1){
+        if (board[0] == marker && board[2] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[4] == marker && board[7] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+    else if (squareNum == 2){
+        if (board[1] == marker && board[0] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[4] == marker && board[6] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[5] == marker && board[8] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+    else if (squareNum == 3){
+        if (board[4] == marker && board[5] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[0] == marker && board[6] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+    else if (squareNum == 4){
+        if (board[1] == marker && board[7] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[3] == marker && board[5] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[0] == marker && board[8] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[2] == marker && board[6] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+    else if (squareNum == 5){
+        if (board[2] == marker && board[8] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[4] == marker && board[3] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+    else if (squareNum == 6){
+        if (board[3] == marker && board[0] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[2] == marker && board[4] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[7] == marker && board[8] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+    else if (squareNum == 7){
+        if (board[1] == marker && board[4] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[6] == marker && board[8] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+    else if (squareNum == 8){
+        if (board[3] == marker && board[0] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[2] == marker && board[5] == marker){
+            console.log("You Win!")
+            gameStatus()
+        } else if (board[6] == marker && board[7] == marker){
+            console.log("You Win!")
+            gameStatus()
+        }
+    }
+}
+
+
 
 /* Changing player after every click and linking with other functions */
 var test = function(){
     var value = this.getAttribute("value");
     if (player === "X"){
+        /*Locking user input in place */
+        if (board[value] === " "){
         gameBoard.playerMove(value,"X")
-        playerCurrentX.displayChange(value, "X");
+        playerCurrent.displayChange(value, "X"); 
         player = "O"
-        console.log()   
-    } else {
+        check(value, "X")
+        console.log()
+        console.log(gameState)
+        } else {
+            return
+        }
+    } else  {
+        /*Locking user input in place */
+        if (board[value] === " "){
         gameBoard.playerMove(value,"O")
-        playerCurrentO.displayChange(value, "O");
+        playerCurrent.displayChange(value, "O");
+        check(value, "O")
         player = "X"
         console.log()
+        console.log(gameState)
+        } else{
+            return
+        }
     }
     
     console.log(board)
     return board
 }
-/* addEvenListener to get clicks */
-for (var i = 0; i < CELL.length; i++) {
-    CELL[i].addEventListener('click', test, false);
+var end = function end() {
+    console.log(gameState)
+}
+/* addEventListener to get clicks */
+if (gameState == "playing"){
+    for (var i = 0; i < CELL.length; i++) {
+        CELL[i].addEventListener('click', test, false);
+    }
+}else if (gameState === "end"){
+    for (var i = 0; i < CELL.length; i++) {
+        CELL[i].removeEventListener('click', end, false);
+    }
 }
 
 /* Testing
